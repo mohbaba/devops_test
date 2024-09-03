00fda749b019f5f85103f0db8194d89ab79dea5f
Muhammad Baba Muhammad <59345145+mohbaba@users.noreply.github.com>
2024-09-03 14:45:34 +0100

    Hello

--- src/main/java/com/example/hello_world/controllers/HelloWorldController.java
+++ src/main/java/com/example/hello_world/controllers/HelloWorldController.java
@@ -1,13 +1,15 @@
 package com.example.hello_world.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/helloworld")
+@RequestMapping("api")
 @RestController
 public class HelloWorldController {
 
+    @GetMapping("/helloworld")
     public ResponseEntity<?> response(){
         return ResponseEntity.ok("Hello, World!!");
 
